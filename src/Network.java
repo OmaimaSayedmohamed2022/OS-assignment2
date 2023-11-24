01faef700027a21a190643401df827a82d5d9b4c
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2023-11-24 17:19:07 +0200

    Update Network.java

--- src/Network.java
+++ src/Network.java
@@ -1,6 +1,7 @@
-import java.io.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.io.FileWriter;
+import java.io.IOException;
 
 class Router {
     private List<Connection> connections;
@@ -15,8 +16,8 @@ class Router {
         connectionSemaphore.P(); // Acquire a connection permit
         Connection connection = new Connection(device);
         connections.add(connection);
-        System.out.println("- " + device.getDeviceName() + "(" + device.getType() + ") arrived");
-        System.out.println("- Connection " + connection.getId() + ": " + device.getDeviceName() + " Occupied");
+        writeToOutput("- " + device.getDeviceName() + "(" + device.getType() + ") arrived");
+        writeToOutput("- Connection " + connection.getId() + ": " + device.getDeviceName() + " Occupied");
     }
 
     public void releaseConnection(Device device) {
@@ -24,9 +25,9 @@ class Router {
         if (connection != null) {
             connections.remove(connection);
             connectionSemaphore.V(); // Release a connection permit
-            System.out.println("- " + device.getDeviceName() + " Logged out");
+            writeToOutput("- " + device.getDeviceName() + " Logged out");
         } else {
-            System.out.println("- " + device.getDeviceName() + " does not have a connection to disconnect.");
+            writeToOutput("- " + device.getDeviceName() + " does not have a connection to disconnect.");
         }
     }
 
@@ -37,6 +38,16 @@ class Router {
             }
         }
         return null;
+    }
+
+    private void writeToOutput(String message) {
+        try {
+            FileWriter writer = new FileWriter("output.txt", true);
+            writer.write(message + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private class Semaphore {
@@ -58,31 +69,25 @@ class Router {
             notify();
         }
     }
-}
 
-class Connection {
-    private static int connectionIdCounter = 1;
-    private int id;
-    private Device device;
+    public class Connection {
+        private static int connectionIdCounter = 1;
+        private int id;
+        private Device device;
 
-    public Connection(Device device) {
-        this.id = connectionIdCounter++;
-        this.device = device;
+        public Connection(Device device) {
+            this.id = connectionIdCounter++;
+            this.device = device;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public Device getDevice() {
+            return device;
+        }
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public Device getDevice() {
-        return device;
-    }
-}
-
-enum Activity {
-    connect,
-    perform_online_activity,
-    disconnect
 }
 
 class Device extends Thread {
@@ -110,20 +115,24 @@ class Device extends Thread {
     public void run() {
         try {
             router.occupyConnection(this);
-            Connection connection = router.findConnection(this);
+            writeToOutput("- Connection " + router.findConnection(this).getId() + ": " + name + " login");
 
-            if (connection != null) {
-                System.out.println("- Connection " + connection.getId() + ": " + name + " login");
+            // Simulate online activity
+            TimeUnit.MILLISECONDS.sleep((long) (Math.random() * 3000));
+            writeToOutput("- Connection " + router.findConnection(this).getId() + ": " + name + " performs online activity");
 
-                // Simulate online activity
-                TimeUnit.MILLISECONDS.sleep((long) (Math.random() * 3000));
-                System.out.println("- Connection " + connection.getId() + ": " + name + " performs online activity");
-
-                router.releaseConnection(this);
-            } else {
-                System.out.println("- " + name + " could not establish a connection.");
-            }
+            router.releaseConnection(this);
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToOutput(String message) {
+        try {
+            FileWriter writer = new FileWriter("output.txt", true);
+            writer.write(message + "\n");
+            writer.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -131,42 +140,29 @@ class Device extends Thread {
 
 public class Network {
     public static void main(String[] args) {
-        try {
-            // Redirect output to a file
-            String fileName = "output.txt";
-            FileOutputStream fos = new FileOutputStream(fileName);
-            PrintStream ps = new PrintStream(fos);
-            System.setOut(ps);
+        Scanner scanner = new Scanner(System.in);
 
-            Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the number of WI-FI Connections?");
+        int maxConnections = scanner.nextInt();
 
-            System.out.println("What is the number of WI-FI Connections?");
-            int maxConnections = scanner.nextInt();
+        System.out.println("What is the number of devices Clients want to connect?");
+        int totalDevices = scanner.nextInt();
 
-            System.out.println("What is the number of devices Clients want to connect?");
-            int totalDevices = scanner.nextInt();
+        Router router = new Router(maxConnections);
+        Device[] devices = new Device[totalDevices];
 
-            Router router = new Router(maxConnections);
-            Device[] devices = new Device[totalDevices];
+        for (int i = 0; i < totalDevices; i++) {
+            System.out.println("Enter the name and type of device : ");
+            String deviceName = scanner.next();
+            String deviceType = scanner.next();
 
-            for (int i = 0; i < totalDevices; i++) {
-                System.out.println("Enter the name and type of device : ");
-                String deviceName = scanner.next();
-                String deviceType = scanner.next();
-
-                devices[i] = new Device(deviceName, deviceType, router, i + 1);
-            }
-
-            for (Device device : devices) {
-                device.start();
-            }
-
-            // Close the redirected output stream
-            fos.close();
-
-            scanner.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            devices[i] = new Device(deviceName, deviceType, router, i + 1);
         }
+
+        for (Device device : devices) {
+            device.start();
+        }
+
+        scanner.close();
     }
 }
